e5e23e79e404c2da0ed1a6988e08c49c0053c444
Thomas Kiley <thomas.kiley@diffblue.com>
2019-10-17 21:27:11 +0100

    Fix wrong module name
    Caused by trying to supress warning from pycharm

--- bio_pronoun_counter/main.py
+++ bio_pronoun_counter/main.py
@@ -36,10 +36,10 @@ def get_handle_from_bio(api, handle):
 
 
 def main(data_path):
-    api = python_twitter.Api(consumer_key=os.environ['CONSUMER_API_KEY'],
-                             consumer_secret=os.environ['CONSUMER_API_SECRET'],
-                             access_token_key=os.environ['ACCESS_TOKEN'],
-                             access_token_secret=os.environ['ACCESS_SECRET'])
+    api = twitter.Api(consumer_key=os.environ['CONSUMER_API_KEY'],
+                      consumer_secret=os.environ['CONSUMER_API_SECRET'],
+                      access_token_key=os.environ['ACCESS_TOKEN'],
+                      access_token_secret=os.environ['ACCESS_SECRET'])
 
     yes_count = 0
     total_count = 0
